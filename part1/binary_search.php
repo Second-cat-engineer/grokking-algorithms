03c39a42353e64a574a0f12a147a4b6c86758dbe
Yaylyaev Safuan <yaylyaevsafuan@gmail.com>
2021-07-26 07:22:23 +0300

    +part1

---
+++ part1/binary_search.php
@@ -0,0 +1,26 @@
+<?php
+
+function binary_search($list, $item) {
+    $start = 0;
+    $end = count($list) - 1;
+
+    while ($start <= $end) {
+        $mid = floor(($start + $end) / 2);
+
+        $result = $list[$mid];
+
+        if ($result === $item) {
+            return $mid;
+        } elseif ( $result > $item) {
+            $end = $mid - 1;
+        } else {
+            $start = $mid + 1;
+        }
+    }
+    return 'no search';
+}
+
+$my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+echo binary_search($my_list, 4); // 3
+echo binary_search($my_list, -1); // no search
