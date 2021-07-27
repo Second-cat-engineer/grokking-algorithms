c7e8ac039b21fd1c621fb7b3efca7e109b37bfba
Yaylyaev Safuan <yaylyaevsafuan@gmail.com>
2021-07-27 23:33:19 +0300

    +part5/6

---
+++ part6/bread_first_search.php
@@ -0,0 +1,49 @@
+<?php
+
+$graph = [];
+
+$graph['you'] = ['alice', 'bob', 'claire'];
+$graph['bob'] = ['anuj', 'peggy'];
+$graph['alice'] = ['peggy'];
+$graph['claire'] = ['thom', 'jonny'];
+$graph['anuj'] = [];
+$graph['peggy'] = [];
+$graph['thom'] = [];
+$graph['jonny'] = [];
+
+
+function breadthFirstSearch($graph, $name) {
+    $searchQueue = [];
+    $searched = [];
+
+    foreach ($graph[$name] as $value) {
+        $searchQueue[] = $value;
+    }
+
+    while ($searchQueue) {
+        $person = array_shift($searchQueue);
+
+        if (!in_array($person, $searched)) {
+
+            if (personIsSeller($person)) {
+
+                return $person . ' is a seller!';
+
+            } else {
+
+                foreach ($graph[$person] as $value) {
+                    $searchQueue[] = $value;
+                }
+            }
+        }
+    }
+
+    return 'seller not found';
+}
+
+function personIsSeller($person) {
+    return 'thom' === $person;
+}
+
+echo breadthFirstSearch($graph, 'you');
+echo breadthFirstSearch($graph, 'alice');
